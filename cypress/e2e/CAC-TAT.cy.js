0711b39ec4b744664d59023275576335bc2ecdf1
Igor <igorserrado@gmail.com>
2025-05-26 21:14:05 -0300

    Licão 3 e 4

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,11 +10,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
-    cy.get('#firstName').type('Irgo', {delay:100})
-    cy.get('#lastName').type('Serrado')
-    cy.get('#email').type('igorserrado@gmail.com')
-    cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('div', 'Nome').type('Irgo', {delay:100})
+    cy.contains('div', 'Sobrenome').type('Serrado')
+    cy.contains('div', 'E-mail').type('igorserrado@gmail.com')
+    cy.contains('div', 'ajudar').type('Teste')
+    cy.contains('button', 'Enviar').click()
     
     cy.get('.success').should('be.visible')
   })
@@ -23,7 +23,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Serrado')
     cy.get('#email').type('igorserradogmail.com')
     cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
     
     cy.get('.error').should('be.visible')
   })
@@ -40,25 +40,59 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#email').type('igorserradogmail.com')
     cy.get('#open-text-area').type('Teste')
     cy.get('#phone-checkbox').check()
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
     
     cy.get('.error').should('be.visible')
 
     })
 
-    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
     cy.get('#firstName').type('Irgo').should('have.value', 'Irgo').clear().should('have.value', '')
     cy.get('#lastName').type('Serrado').should('have.value', 'Serrado').clear().should('have.value', '')
     cy.get('#email').type('igorserrado@gmail.com').should('have.value', 'igorserrado@gmail.com').clear().should('have.value', '')
     cy.get('#phone').type('12345678').should('have.value', '12345678').clear().should('have.value', '')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-      cy.get('button[type="submit"]').click()
+      cy.contains('button', 'Enviar').click()
       
       cy.get('.error').should('be.visible')
     })
-})
+
+    it('envia o formulário com sucesso usando um comando customizado', () => {
+      const data = {
+        firstName: 'Irgo',
+        lastName: 'Serrado',
+        email: 'igorserrado@gmail.com',
+        text: 'Teste'
+      }	
+      
+      cy.fillMandatoryFieldsAndSubmit(data)
+      cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', () => {
+      cy.get('#product').select('YouTube').should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+      cy.get('#product').select('mentoria').should('have.value', 'mentoria') 
+    })
+
+    it('seleciona um produto (Blog) por seu índice', () => {
+      cy.get('select').select(1).should('have.value', 'blog') 
+    })
+
+    it('marca o tipo de atendimento "Feedback"', () => {
+      cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')
+    })
+
+    it.only('marca cada tipo de atendimento', () => {
+      cy.get('input[type="Radio"]').each(typeOfService => {
+        cy.wrap(typeOfService).check().should('be.checked')
+      })
+    })
+  })
